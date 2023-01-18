fef50694845356c58db1e52fe38ba81b23a20133
SergeiSah <sahhome@yandex.ru>
2023-01-18 22:29:41 +0300

    Added metadata to the header of saving data

--- scans.py
+++ scans.py
@@ -11,23 +11,32 @@ class Scan:
 
     def __init__(self, rsm: Bucket):
         self.rsm = rsm
-        self.results = pd.DataFrame(columns=['x_scale', 'counter_1', 'counter_2'])
         self.path_to_files = Settings().path_for_files_save
+        self.results = None
+
+        self.initial_state()
+
+    def initial_state(self):
+        self.results = pd.DataFrame(columns=['x_scale', 'counter_1', 'counter_2'])
+        self.rsm.motor_select(4)    # remove voltage from all motors
 
     def en_scan(self, direction: int, exposure: int, steps_num: int, step_rev: float, start_rev: float):
+        meta = {'scan_type': 'energy scan',
+                'exposure': f'{exposure} s'}
         self.rsm.motor_select(MOTOR_0)
         self.results.rename(columns={'x_scale': 'rev'}, inplace=True)
-        sign = {1: 1, 0: -1}[direction]
-        file_num = self.max_file_number()
+        sign = {1: 1, 0: -1}[direction]     # 1 - rev increasing, 0 - rev decreasing
+        file_num = self.max_file_number()   # determine the file number to save results
 
         for step_num in range(steps_num + 1):
             data = self.measurement(exposure)
 
-            # Attempt to realise interruption
+            # if the measurement was interrupted - stop scan
             if data is None:
                 break
 
             self.rsm.motor_move(direction, rev_to_steps(step_rev))
+            # if the motor moving was interrupted - stop scan
             if not self.rsm.motor_moving():
                 break
 
@@ -36,9 +45,10 @@ class Scan:
             x_scale = pd.DataFrame({'rev': [rev]})
             data = pd.concat([x_scale, data], axis=1)
             self.results = pd.concat([self.results, data])
-            self.save_results(file_num)
 
-        self.rsm.motor_select(4)
+            self.save_results(file_num, meta)
+
+        self.initial_state()
 
     def dscan(self):
         pass
@@ -52,21 +62,35 @@ class Scan:
     def measurement(self, exposure: int):
         self.rsm.exposure_set(exposure * 10)
         self.rsm.counter_start()
-        if self.rsm.counter_working():
+        if self.rsm.counter_working():  # if the measurement was not interrupted, return the data obtained
             return pd.DataFrame({'counter_1': [self.rsm.counter_get(FIRST_COUNTER)],
                                  'counter_2': [self.rsm.counter_get(SECOND_COUNTER)]})
         return None
 
     def max_file_number(self):
+        # list of all .txt files in the directory with data files
         data_files = list(filter(lambda f: '.txt' in f, os.listdir(self.path_to_files)))
+        # list of file numbers: DM_{number}.txt
         nums_list = list(map(lambda f: int(re.findall(r"DM_(\d*).txt", f)[0]), data_files))
+
         if len(nums_list) == 0:
             return 0
         return max(nums_list)
 
-    def save_results(self, file_num):
+    def save_results(self, file_num: int, meta_data: dict):
+        # form new file name: DM_{four digits}.txt, for example: DM_0012.txt
         new_file = f'DM_{str.zfill(str(file_num + 1), 4)}.txt'
-        self.results.to_csv(self.path_to_files + new_file, sep='\t', index=False)
+
+        # save metadata at the header of the file
+        with open(self.path_to_files + new_file) as file:
+            for key, value in meta_data.items():
+                file.write(f'# {key}: {value:>15}\n')   # definition of metadata string style
+
+        self.results.to_csv(self.path_to_files + new_file,
+                            sep='\t',
+                            index=False,
+                            mode='a',
+                            float_format='%.3f')
 
 
 if __name__ == '__main__':
