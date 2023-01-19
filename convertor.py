106cda85f68b66983cf7c46b9519cb4cd13a45a2
SergeiSah <sahhome@yandex.ru>
2023-01-19 15:46:50 +0300

    Function to_step was added to the convertor.py. To save signature of a wrapped function @wrap() decorator was added to the arguments_type_checker_in_class().

--- convertor.py
+++ convertor.py
@@ -12,3 +12,10 @@ def step_to_revs(step: int) -> float:
 
 def step_to_grads(step: int) -> float:
     return round(step / 8000 * 90, 2)
+
+
+def to_step(motor: int, step: float) -> int:
+    return {
+        0: rev_to_steps(step),
+        **dict.fromkeys([1, 2, 3], grad_to_steps(step))
+    }[motor]
--- handlers.py
+++ handlers.py
@@ -1,8 +1,11 @@
-def arguments_type_checker_in_class(func):
+from functools import wraps
 
+
+def arguments_type_checker_in_class(func):
+    @wraps(func)
     def wrapper(self, *args):
         for arg, arg_type in zip(args, func.__annotations__.items()):
             assert isinstance(arg, arg_type[1]), f"Argument '{arg_type[0]}' must be {arg_type[1]}, not {type(arg)}"
         return func(self, *args)
-    wrapper.__annotations__ = func.__annotations__
+    # wrapper.__annotations__ = func.__annotations__
     return wrapper
