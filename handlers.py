3915756a30aa577aad2233c311df3ff4acd19cb1
SergeiSakhonenkov <sergei.sakhonenkov@gmail.com>
2023-01-25 17:28:31 +0300

    Corrected data type convertors. Added checker of exposure values and documentation to the functions.

--- handlers.py
+++ handlers.py
@@ -1,4 +1,5 @@
 from functools import wraps
+from typing import Union
 
 
 def arguments_type_checker_in_class(func):
@@ -12,21 +13,54 @@ def arguments_type_checker_in_class(func):
     return wrapper
 
 
-def reduce_datatypes_to_func(func, *args):
-    f_param_types = func.__annotations__.values()
-    return reduce_datatypes(f_param_types, *args)
+def convert_datatypes_to_func(func, *args) -> Union[list, None]:
+    """
+    Convert types of the given arguments to the types indicated in a func annotations. If len(args) !=
+    len(f_arguments without defs) - return None. defs - default parameters.
+
+    :param func: some function with annotated parameters
+    :param args: arguments, that will be transferred to the function
+    :return: list of converted arguments or None
+    """
+    f_param_types = list(func.__annotations__.values())  # list of types of the func parameters
+    defaults = func.__defaults__                         # tuple of the default parameters of the func or None
+    defaults = list(defaults) if defaults is not None else []
+    len_pos_args = len(f_param_types) - len(defaults)    # length of the positional arguments of the func
+
+    if len(f_param_types) < len(args) or len(args) < len(f_param_types) - len(defaults):
+        print('Wrong number of the arguments')
+        return
+
+    _args = list(args)
+    _args.extend(defaults[len(_args) - len_pos_args:])  # add necessary default values
+    return convert_datatypes(f_param_types, *_args)
 
 
-def reduce_datatypes(types: list, *args):
-    if len(args) == 0:
-        return None
-    elif len(types) != len(args):
-        print('Wrong number of arguments')
-        return None
+def convert_datatypes(types: list, *args) -> Union[list, None]:
+    """
+    Convert given types of values to indicated in the list `types`. len(list) must be == len(args)
 
+    :param types: list of types
+    :param args: values
+    :return: list of converted values, or None
+    """
+    if len(args) == 0:  # Case of functions without arguments
+        return []
     try:
         return list(map(lambda x, y: x(y), types, args))
     except ValueError:
         print('Invalid value of an argument')
-        return None
+        return
 
+
+def check_exposure(exposure: Union[int, float]) -> bool:
+    """
+    Exposure must be set between 0.1 and 999. Check given value.
+
+    :param exposure: exposure in seconds
+    :return: True if the value in the range, else False
+    """
+    if 9999 >= int(exposure * 10) >= 1:
+        return True
+    print('Invalid value of the exposure. It must be in the range [0.1, 999]')
+    return False
