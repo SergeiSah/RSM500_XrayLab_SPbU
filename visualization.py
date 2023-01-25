c6c6c9c946c981f3afd491f4813372a3d3edae48
SergeiSakhonenkov <sergei.sakhonenkov@gmail.com>
2023-01-25 17:31:53 +0300

    Figure display was changed

--- visualization.py
+++ visualization.py
@@ -5,8 +5,17 @@ import matplotlib.pyplot as plt
 
 class Plotter:
     labels = {
-        'en_scan': {'x_scale': 'Revs of the reel', 'y_scale': 'Counts'}
+        'escan': {'x_scale': 'Revs of the reel', 'y_scale': 'Counts'},
+        'mscan': {'x_scale': 'Time in seconds', 'y_scale': 'CPS'}
     }
+    font_sizes = {
+        'axis': 22,
+        'title': 26,
+        'ticks': 15,
+        'legend': 16
+    }
+
+    Y_MARGIN = 0.05
 
     @staticmethod
     def terminate():
@@ -19,23 +28,38 @@ class Plotter:
                 self.terminate()
                 return False
             else:
-                self.axs[0].plot(df.iloc[:, 0], df.iloc[:, 1], 'r-')
-                self.axs[1].plot(df.iloc[:, 0], df.iloc[:, 2], 'r-')
-        self.fig.canvas.draw()
+                self.axs[0].plot(df.index, df.iloc[:, 1], 'r-', label='Detector 2')
+
+                if df.shape[0] == 1:
+                    continue
+
+                self.axs[0].set_xlim([df.index[0], df.index[-1]])
+                # self.axs[0].set_ylim([0 - self.Y_MARGIN * max(df.iloc[1]), max(df.iloc[1]) * (1 + self.Y_MARGIN)])
+                # self.axs[1].plot(df.index, df.iloc[:, 1], 'r-')
+                # self.axs[1].set_xlim([df.index[0], df.index[-1]])
+        self.figure.canvas.draw()
         return True
 
     def __call__(self, pipe: Pipe, scan_mode: str):
         self.pipe = pipe
-        # names = self.labels[scan_mode]
+        names = self.labels[scan_mode]
+        self.figure = plt.figure(figsize=(10, 8))
+        self.axs = []
+        self.axs.append(self.figure.add_subplot(1, 1, 1))
 
-        self.fig, self.axs = plt.subplots(nrows=2, ncols=1)
+        self.axs[0].set_title(scan_mode, fontsize=self.font_sizes['title'])
+        self.axs[0].set_ylabel(names['y_scale'], fontsize=self.font_sizes['axis'])
+        self.axs[0].set_xlabel(names['x_scale'], fontsize=self.font_sizes['axis'])
+        self.axs[0].grid(True, linestyle='--')
 
-        self.axs[0].set_title('Detector 1')
-        # self.axs[0].set_ylabel(names['y_label'])
-        self.axs[1].set_title('Detector 2')
-        # self.axs[1].set_xlabel(names['x_label'])
+        self.axs[0].plot([], [], 'r-', label='Detector 2')
+        self.axs[0].legend(fontsize=self.font_sizes['legend'])
 
-        timer = self.fig.canvas.new_timer(interval=100)
+        plt.xticks(fontsize=self.font_sizes['ticks'])
+        plt.yticks(fontsize=self.font_sizes['ticks'])
+
+        timer = self.figure.canvas.new_timer(interval=100)
+        self.figure.canvas.set_window_title(f'RSM-controller: {scan_mode}')
         timer.add_callback(self.update_plot)
         timer.start()
 
