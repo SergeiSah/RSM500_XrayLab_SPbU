53e1fcbc9d94bc97599d5ec23777a1641c2d2e8d
SergeiSakhonenkov <sergei.sakhonenkov@gmail.com>
2023-01-26 13:58:45 +0300

    Added documentation

--- main.py
+++ main.py
@@ -8,7 +8,15 @@ from settings import Settings
 
 
 def main():
-    s = Settings()
+    """
+    Program works as a command prompt. User should input commands in a form 'mode <parameter> [parameter...]'. All
+    available modes are presented in the CommandRunner().modes dictionary of the command_run.py module. To close the
+    program, one should input 'close', 'quit', 'exit', 'c' or 'q'.
+
+    :return: None
+    """
+    s = Settings()  # global settings of the program
+    # converts command to byte code and sends it to the controllers of the bucket of RSM
     rsm = Bucket(serial.Serial(port=s.port, baudrate=s.baudrate))
     cr = CommandRunner(rsm)
 
@@ -16,7 +24,7 @@ def main():
         # extraction from the command the mode name and arguments
         mode, *args = re.sub('\s+', ' ', input('> ').strip()).split(' ')
 
-        if mode in ['close', 'quit', 'c', 'q']:
+        if mode in ['close', 'quit', 'exit', 'c', 'q']:
             break
 
         cr.run_command(mode, *args)
