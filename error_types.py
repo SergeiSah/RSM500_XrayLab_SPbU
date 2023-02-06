a4d0614fafd0b67e47c3f34360d34b186ae2a06a
SergeiSakhonenkov <sergei.sakhonenkov@gmail.com>
2023-02-06 11:50:12 +0300

    Added custom exception and new validations

---
+++ error_types.py
@@ -0,0 +1,10 @@
+class MotorException(Exception):
+    pass
+
+
+class DetectorException(Exception):
+    pass
+
+
+class PlotException(Exception):
+    pass
--- handlers.py
+++ handlers.py
@@ -3,15 +3,17 @@ from inspect import signature
 from typing import Union
 
 from definitions import *
+from error_types import *
 
 
 def arguments_type_checker_in_class(func):
     @wraps(func)    # save signature of the function
     def wrapper(self, *args):
-        # reduced_args = []
+        assert hasattr(func, '__annotations__'), f'Hey, "programmer", you forgot annotate your function {func}'
+
         for arg, arg_type in zip(args, func.__annotations__.items()):
-            assert isinstance(arg, arg_type[1]), f"Argument '{arg_type[0]}' must be {arg_type[1]}, not {type(arg)}"
-            # reduced_args.append(arg_type(arg))
+            if not isinstance(arg, arg_type[1]):
+                raise TypeError(f"Argument '{arg_type[0]}' must be {arg_type[1]}, not {type(arg)}")
         return func(self, *args)
     return wrapper
 
@@ -33,7 +35,7 @@ def convert_datatypes_to_func(func, *args) -> Union[list, None]:
     if len(args) == 0 and len(f_param_types) > 0:
         return
     elif len(f_param_types) < len(args) or len(args) < len(f_param_types) - len(defaults):
-        print('Wrong number of the arguments')
+        print('Wrong number of the arguments.')
         return
 
     _args = list(args)
@@ -51,29 +53,22 @@ def convert_datatypes(types: list, *args) -> Union[list, None]:
     """
     if len(args) == 0:  # Case of functions without arguments
         return []
-    try:
+    try:    # TODO: raise
         return list(map(lambda x, y: x(y), types, args))
     except ValueError:
         print('Invalid value of an argument')
-        return
 
 
-def scan_check_params_and_log(scan_func):
+def validate_and_log(scan_func):
     @wraps(scan_func)
     def wrapper(self, *args, **kwargs):
         f_params = {name: val for name, val in zip(signature(scan_func).parameters, [self, *args])}
-        try:
-            if 'motor' in f_params:
-                assert f_params['motor'] != MOTOR_0, f'Command works only for the motors {MOTOR_1}, ' \
-                                                     f'{MOTOR_2} and {MOTOR_3}.'
-                assert f_params['motor'] in [MOTOR_1, MOTOR_2, MOTOR_3], 'Invalid number of the motor.'
-            assert f_params['step_num'] > 0, 'Number of steps cannot be less than 0'
-            assert 1 <= int(f_params['exposure'] * 10) <= 9999, f'Invalid exposure {f_params["exposure"]}, ' \
-                                                                f'must be in the range of [0.1, 999]'
-            # TODO: add assert for step and start/end positions
-        except AssertionError as msg:
-            print(msg)
-            return -1
+        if 'motor' in f_params:
+            validate_motor(f_params['motor'], scan_func.__name__)
+        if not f_params['step_num'] > 0:
+            raise ValueError('Number of steps cannot be less than 0.')
+        validate_exposure(f_params['exposure'])
+        # TODO: add raise error for step and start/end positions
 
         f_params.pop('self', None)
         self.log.info(f'Start [{scan_func.__name__}]' +
@@ -91,3 +86,20 @@ def scan_check_params_and_log(scan_func):
         return was_stopped
     return wrapper
 
+
+def validate_motor(motor: int, scan_func_name=None):
+    motors = [MOTOR_0, MOTOR_1, MOTOR_2, MOTOR_3]
+
+    if scan_func_name is not None:
+        if not motor != MOTOR_0:
+            raise MotorException(f'Command [{scan_func_name}] works only with the motors '
+                                 f'{MOTOR_1}, {MOTOR_2} and {MOTOR_3}.')
+        motors.remove(MOTOR_0)
+
+    if motor not in motors:
+        raise MotorException('Invalid number of the motor.')
+
+
+def validate_exposure(exposure: float):
+    if not 1 <= int(exposure * 10) <= 9999:
+        raise DetectorException(f'Exposure must be in the range of [0.1, 999], your value is {exposure}.')
