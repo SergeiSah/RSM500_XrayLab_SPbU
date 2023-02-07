dca9bb5c448a9b18dc17ce05aeebb2eb676ea4eb
SergeiSakhonenkov <sergei.sakhonenkov@gmail.com>
2023-02-07 14:46:22 +0300

    Settings were reformatted to be more readable

--- config/settings.py
+++ config/settings.py
@@ -7,80 +7,47 @@ class Settings:
     path_to_settings_ini = os.path.join(ROOT_DIR, r'config\settings.ini')
 
     def __init__(self):
-        self.__config = configparser.ConfigParser()
-        self.__config.read(self.path_to_settings_ini)
+        self._config = configparser.ConfigParser()
+        self._config.read(self.path_to_settings_ini)
 
     @property
     def port(self):
-        return self.__config['DEFAULT']['port']
+        return self._config['CONTROLLER_CONNECTION']['port']
 
     @port.setter
     def port(self, value: str):
-        self.__config['DEFAULT']['port'] = value
-        self.write_changes_to_settings()
+        self._config['CONTROLLER_CONNECTION']['port'] = value
+        self.save_changes()
 
     @property
     def baudrate(self):
-        return self.__config['DEFAULT']['baudrate']
+        return self._config['CONTROLLER_CONNECTION']['baudrate']
 
     @baudrate.setter
-    def baudrate(self, value):
-        self.__config['DEFAULT']['baudrate'] = value
-        self.write_changes_to_settings()
+    def baudrate(self, value: int):
+        self._config['CONTROLLER_CONNECTION']['baudrate'] = str(value)
+        self.save_changes()
 
     @property
-    def path_for_files_save(self):
-        return self.__config['PATHS']['path_for_files_save']
+    def path_to_datafiles(self):
+        return self._config['PATHS']['path_to_datafiles']
 
-    @path_for_files_save.setter
-    def path_for_files_save(self, path: str):
-        self.__config['PATHS']['path_for_files_save'] = path
-        self.write_changes_to_settings()
+    @path_to_datafiles.setter
+    def path_to_datafiles(self, path: str):
+        self._config['PATHS']['path_to_datafiles'] = path
+        self.save_changes()
 
-    # absolute positions of the motors 1, 2, 3
-    @property
-    def apos_motor_1(self):
-        return self.__config['ABS_MOTOR_POSITIONS']['motor_1']
+    def get_abs_motor_position(self, motor_num: int) -> int:
+        return int(self._config['ABSOLUTE_MOTOR_POSITION'][f'motor_{motor_num}'])
 
-    @apos_motor_1.setter
-    def apos_motor_1(self, value):
-        self.__config['ABS_MOTOR_POSITIONS']['motor_1'] = value
-        self.write_changes_to_settings()
+    def set_abs_motor_position(self, motor_num: int, value: int) -> None:
+        self._config['ABSOLUTE_MOTOR_POSITION'][f'motor_{motor_num}'] = \
+            str(int(self._config['ABSOLUTE_MOTOR_POSITION'][f'motor_{motor_num}']) + value)
+        self.save_changes()
 
-    @property
-    def apos_motor_2(self):
-        return self.__config['ABS_MOTOR_POSITIONS']['motor_2']
+    def get_limits(self, motor_num: int) -> tuple:
+        return tuple(map(int, self._config['LIMITS'][f'motor_{motor_num}'].split(' ')))
 
-    @apos_motor_2.setter
-    def apos_motor_2(self, value):
-        self.__config['ABS_MOTOR_POSITIONS']['motor_2'] = value
-        self.write_changes_to_settings()
-
-    @property
-    def apos_motor_3(self):
-        return self.__config['ABS_MOTOR_POSITIONS']['motor_3']
-
-    @apos_motor_3.setter
-    def apos_motor_3(self, value):
-        self.__config['ABS_MOTOR_POSITIONS']['motor_3'] = value
-        self.write_changes_to_settings()
-
-    def write_changes_to_settings(self):
+    def save_changes(self):
         with open(self.path_to_settings_ini, 'w') as configfile:
-            self.__config.write(configfile)
-
-    def change_motor_apos(self, motor_num: int, value: int):
-        if motor_num == MOTOR_1:
-            self.apos_motor_1 = str(int(self.apos_motor_1) + value)
-        elif motor_num == MOTOR_2:
-            self.apos_motor_2 = str(int(self.apos_motor_2) + value)
-        elif motor_num == MOTOR_3:
-            self.apos_motor_2 = str(int(self.apos_motor_3) + value)
-        return 0
-
-    def get_motor_apos(self, motor_num: int) -> str:
-        return {
-            MOTOR_1: self.apos_motor_1,
-            MOTOR_2: self.apos_motor_2,
-            MOTOR_3: self.apos_motor_3
-        }[motor_num]
+            self._config.write(configfile)
