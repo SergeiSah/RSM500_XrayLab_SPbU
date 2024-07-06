fe3e95c7ea799aadac821aba576bbbd7a7b034de
SergeiSakhonenkov <sergei.sakhonenkov@gmail.com>
2024-07-06 12:37:54 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='RSM-controller',
+    version='0.1.0',
+    packages=['config', 'rsm500'],
+    url='https://github.com/SergeiSah/RSM500_XrayLab_SPbU',
+    license='',
+    author='Sergei Sakhonenkov',
+    author_email='sergei.sakhonenkov@gmail.com',
+    description='The control program of the X-ray spectrometer monochromator '
+                'RSM-500 in the X-ray laboratory of Saint-Petersburg State University'
+)
